3c3e3dd58cbbcb446c4951f14f927a2edf5008a6
Admin <admin@MacBook-Pro.local>
2019-06-26 23:35:17 -0400

    Updates

---
+++ precache-manifest.1cc0d8f07d7a7027339f49cd4152ef61.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fa565d55baa11c4bbf71c486c51e04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "cd906acb5ba64bd8e3e3",
+    "url": "/static/js/main.4ac6db2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
